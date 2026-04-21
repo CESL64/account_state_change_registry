d57b044572e6f8a0aca982c17992a3c474dfc7b6
CESL64 <carl.salinas64@gmail.com>
2026-04-21 11:53:29 -0600

    [ADD] Reporte PDF de cambios de estado

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
     "version": "18.0.1.0.0",
     "summary": "Registro de cambios de estado para facturas",
     "description": "Hereda account.move para registrar cambios en state.",
-    "author": "Prueba Tecnica",
+    "author": "Carlos Eduardo Salinas Honduras",
     "license": "LGPL-3",
     "category": "Accounting",
     "depends": [
@@ -13,6 +13,7 @@
     "data": [
         "views/state_change_registry_views.xml",
         "views/account_move_views.xml",
+        "reports/account_move_state_change_registry_report.xml",
     ],
     "installable": True,
     "application": False,
